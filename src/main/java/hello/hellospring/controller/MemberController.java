297ce46f644ba53f0dd902dd49b6a88e411a2be5
JinYeongKim <handayoung11@gmail.com>
2022-04-15 17:53:49 +0900

    member 등록을 위한 페이지 작성

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -13,4 +14,10 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createMember() {
+        return "members/newMember";
+    }
+
 }
