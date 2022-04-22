667f739fdfeb59def675881b952fc9b0e64b00f1
JinYeongKim <handayoung11@gmail.com>
2022-04-23 01:21:49 +0900

    SpringDataJpaRepository 생성 및 MemberRepository bean 구현체로 설정

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,31 +1,21 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.MemberJpaRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-
 @Configuration
 public class SpringConfig {
 
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemberMemoryRepository();
-//        return new MemberJdbcTemplateRepository(dataSource);
-        return new MemberJpaRepository(em);
+        return new MemberService(memberRepository);
     }
 }
